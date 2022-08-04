34ce4eb917c40dc972d2b0eaa2476916560d350f
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2022-08-04 14:15:42 -0300

    Logica dias calendario

--- front/src/Pages/Main/index.tsx
+++ front/src/Pages/Main/index.tsx
@@ -12,32 +12,75 @@ const MainPage: FC = () => {
 export default MainPage;
 
 const Calendario: FC = () => {
-    const rows: number[] = [];
-    const dias = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'];
+    const date = new Date();
 
-    for (let i = 1; i <= 31; i++) {
-        rows.push(i);
+    const ultimoDiaMesAtual = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const ultimoDiaMesPassado = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
+
+    const diasAnteriores: number[] = [];
+    const numerosDias: number[] = [];
+
+    const nomesDias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    const nomesMeses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+    for (let x = 0; x < ultimoDiaMesPassado + 1; x++) {
+        diasAnteriores.push(x);
+    }
+
+    for (let i = 1; i <= ultimoDiaMesAtual; i++) {
+        numerosDias.push(i);
     }
 
     return (
         <div
-            style={{
-                width: "60vw",
-                display: "grid",
-                gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
-                gridTemplateRows: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-                gap: "5px",
-            }}
+            style={{ display: "flex", flexDirection: "column", minWidth: "100vw", placeContent: "center" }}
         >
-            {rows.map(x => {
-                return (
-                    <GridCell
-                        key={x}
-                        text={x.toString()}
-                    />
-                )
-            })}
+            <div
+                style={{
+                    width: "100%",
+                    height: "100px",
+                    display: "flex",
+                    placeContent: "center"
+                }}
+            >
+                <h3 style={{ placeSelf: "center" }}>
+                    {nomesMeses[date.getMonth()]}
+                </h3>
+            </div>
 
+
+            <div
+                style={{
+                    width: "fit-content",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(7, 1fr)",
+                    gap: "5px",
+                    placeSelf: "center",
+                }}
+            >
+                {nomesDias.map(x => {
+                    return (
+                        <GridDayTitleCell
+                            key={x}
+                            text={x.toString()}
+                        />
+                    )
+                })}
+
+                {diasAnteriores.map(x => {
+                    return (<div key={x}></div>)
+                })}
+
+                {numerosDias.map(x => {
+                    return (
+                        <GridDayNumberCell
+                            key={x}
+                            text={x.toString()}
+                        />
+                    )
+                })}
+
+            </div>
         </div>
     )
 }
@@ -46,7 +89,24 @@ type GridCellProps = {
     text: string;
 }
 
-const GridCell: FC<GridCellProps> = ({ text }) => {
+const GridDayTitleCell: FC<GridCellProps> = ({ text }) => {
+    return (
+        <div
+            style={{
+                border: "3px solid red",
+                width: "100%",
+                display: "flex",
+                placeItems: "center",
+                placeContent: "center",
+                height: "50px",
+                cursor: "pointer"
+            }}>
+            {text}
+        </div>
+    )
+}
+
+const GridDayNumberCell: FC<GridCellProps> = ({ text }) => {
     return (
         <div
             style={{
